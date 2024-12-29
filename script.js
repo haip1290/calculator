7a591ab4ccbbc44df9aab9f7c81aef5370f113ad
Hai Pham <thanh_hai1290@yahoo.com>
2024-12-29 15:06:14 +0700

    Add calulator functions

--- script.js
+++ script.js
@@ -1,3 +1,61 @@
+const operators = document.querySelectorAll("#operators button");
+const numbers = document.querySelectorAll("#numbers button");
+const equalBtn = document.querySelector("button.equal");
+const display = document.querySelector("#display");
+const clearBtn = document.querySelector("#clear");
+let a, b;
+let operator = "";
+let isA = true;
+let toClear = false;
+// add event to display operator
+operators.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    console.log(`operator:${operator} choice: ${choice.textContent}`);
+    if (operator == choice.textContent || b == undefined) {
+      toClear = true;
+      return;
+    }
+    operator = choice.textContent;
+    isA = false;
+    toClear = true;
+  });
+});
+// add event to display numbers
+numbers.forEach((choice) => {
+  choice.addEventListener("click", () => {
+    if (toClear) {
+      display.textContent = "";
+      toClear = false;
+    }
+    display.textContent += choice.textContent;
+    if (isA) {
+      a = display.textContent;
+      console.log(`a = ${a}`);
+    } else {
+      b = display.textContent;
+      console.log(`b = ${b}`);
+    }
+  });
+});
+// add event to equalBtn
+equalBtn.addEventListener("click", () => {
+  display.textContent = operate(operator, +a, +b);
+  a = +display.textContent;
+  b = undefined;
+  toClear = true;
+});
+
+// add event to clear
+clearBtn.addEventListener("click", clearAll);
+
+function clearAll() {
+  display.textContent = "";
+  a = undefined;
+  b = undefined;
+  operator = "";
+  isA = true;
+  toClear = false;
+}
 function add(a, b) {
   return a + b;
 }
@@ -12,6 +70,7 @@ function divide(a, b) {
 }
 
 function operate(operator, a, b) {
+  console.log(`${a}${operator}${b}`);
   switch (operator) {
     case "+":
       return add(a, b);
